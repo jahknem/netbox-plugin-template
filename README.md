6ca3bfd8b4a97a87ff9be835d3d02d7778b071ef
Jan Kühnemund <jan@kuehnemund.io>
2023-01-13 12:03:09 +0100

    Delete template contents if plugin dir is pwd

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ This is a template for creating a new plugin for [NetBox](https://github.com/net
 The template is based on the devcontainer setup from [NetBox ACL Plugin](https://github.com/ryanmerolle/netbox-acls) by [ryanmerolle](https://github.com/ryanmerolle)
 
 It uses devcontainers used by VSCode to create a devcontainer environment in which one can develop and test netbox plugins. To use it VSCode and Docker have to be installed. This Repo has been tested with Docker Desktop using WSL2 on Windows 10. Other Installations may or may not work.
-To use this template, simply clone it, run the setup.py and run the main.py. This will create a new directory with the name of your plugin. 
+To use this template, simply clone it, run the setup.py and run the main.py. This will create a new plugin structure with the name of your plugin in the current directory. The existing template files will be deleted during this process. 
 
 
 ```bash
 git clone
 cd netbox-plugin-template
-python setup.py install
+pip3 install -r requirements.txt
 python main.py
 ```
 It will then ask you for relevant variables and create a new directory with the name of your plugin. 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 import click
 from colorama import Fore, Back, Style
+from shutil import rmtree
 import os
 
 
@@ -14,11 +15,7 @@ def hello(name):
     click.echo('Hello %s!' % name)
 
 @cli.command()
-def help():
-    click.echo('Help!')
-
-@cli.command()
-@click.option('--directory', prompt='Target Directory', help='The directory in which the new project should be placed')
+@click.option('--directory', help='The directory in which the new project should be placed')
 @click.option('--plugin_name', prompt='Plugin Name', help='The name of the plugin.')
 @click.option('--netbox_version', prompt='Netbox Version', help='The version of Netbox the plugin is compatible with.')
 @click.option('--author_name', prompt='Author Name', help='The name of the author of the plugin.')
@@ -28,7 +25,21 @@ def help():
 @click.option('--git_plugin_url', prompt='Git Plugin URL', help='The URL of the git repository of the plugin.')
 @click.option('--plugin_description', prompt='Plugin Description', help='The description of the plugin.')
 def build(directory, plugin_name, netbox_version, author_name, author_email, version, plugin_url, git_plugin_url, plugin_description):
+    """
+    This function creates the plugin directory, and then creates the files in the plugin directory using the templates in the templates directory.
+    :param directory: The directory where the plugin will be created. If no directory is specified, the plugin will be created in the current directory.
+    :param plugin_name: The name of the plugin.
+    :param netbox_version: The version of NetBox that the plugin is compatible with.
+    :param author_name: The name of the author of the plugin.
+    :param author_email: The author's email address.
+    :param version: The version of the plugin.
+    :param plugin_url: The URL of the plugin.
+    :param git_plugin_url: The URL of the plugin's Git repository.
+    :param plugin_description: A description of the plugin.
+    """
     env = Environment(loader=FileSystemLoader('templates/'))
+    if not directory:
+        directory = '.'
     path = directory + "/" + plugin_name
     class_name = plugin_name.replace("_", " ").title().replace(" ", "")
     if not os.path.exists(path):
@@ -42,6 +53,19 @@ def build(directory, plugin_name, netbox_version, author_name, author_email, ver
         output = template.render(plugin_name=plugin_name, netbox_version=netbox_version, author_name=author_name, author_email=author_email, version=version, plugin_url=plugin_url, git_plugin_url=git_plugin_url, plugin_description=plugin_description, class_name=class_name)
         with open(filename, "w") as f:
             f.write(output)
+
+    if directory is '.':
+        print(Fore.GREEN + "Plugin created in current directory. Deleting the template files...")
+        template_dir = os.path.dirname(os.path.abspath(__file__)) + "/templates"
+        rmtree(template_dir)
+        os.remove(os.path.dirname(os.path.abspath(__file__)) + "/main.py")
+        os.remove(os.path.dirname(os.path.abspath(__file__)) + "/requirements.txt")
+        # Rename the current Dir to the plugin name
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        parent_dir = os.path.dirname(current_dir)
+        os.rename(current_dir, parent_dir + "/" + plugin_name)
+
+
 
 if __name__ == '__main__':
     cli()
